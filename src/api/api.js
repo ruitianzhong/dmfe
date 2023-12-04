b98a3abdd8425f08d6eade333ed4ae5278df7a8d
ruitianzhong <zhongruitian2003@qq.com>
2023-12-04 11:47:45 +0800

    add apis function in api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,162 @@
 import axios from "axios";
-// just a demo
-export const getAllDrvier = () => {
+
+
+export const login = (form) => {
+    return axios.post({
+        url: "/auth/login",
+        data: form,
+    })
+}
+
+export const logout = () => {
+    return axios.post({
+        url: "/auth/logout",
+    })
+}
+
+export const getAllStops = () => {
     return axios.request({
-        url: "/api",
-        method: "get"
+        url: "/api/line/get-all-stops",
+        method: "get",
+    })
+}
+
+export const getAllLineInfo = () => {
+    return axios.request({
+        url: "/api/line/get-all-line-info",
+        method: "get",
+    })
+}
+
+export const getStopsByLineId = (param) => {
+    return axios.request(
+        {
+            url: "/api/line/get-stops-by-line-id?" + param,
+            method: "get",
+        }
+    )
+}
+
+export const getAllViolationTypes = () => {
+    return axios.request(
+        {
+            url: "/api/violation/types",
+            method: "get",
+        }
+    )
+}
+
+export const getAllBus = () => {
+    return axios.request(
+        {
+            url: "/api/bus/get-all-bus",
+            method: "get"
+        }
+    )
+}
+
+export const getLineCaptainByDriverId = (param) => {
+    return axios.request({
+        url: "/api/driver/get-line-captain-by-driver-id?" + param,
+        method: "get",
+    })
+}
+
+export const getFleetCaptainByDriverId = (param) => {
+    return axios.request(({
+        url: "/api/driver/get-fleet-captain-by-driver-id?" + param,
+        method: "get",
+    }))
+}
+
+export const violationByTimeRangeDriverId = (param) => {
+    return axios.request({
+        url: "/api/violation/violation-by-time-range-driver-id?" + param,
+        method: "get",
+    })
+}
+
+export const violationStatByTimeRangeAndFleetId = (param) => {
+    return axios.request({
+        url: "/api/violation/violation-stat-by-time-range-and-fleet-id?" + param,
+        method: "get",
+    })
+}
+
+export const getDriverCaptainByLineId = (param) => {
+    return axios.request({
+        url: "/api/line/get-line-captain-by-line-id?" + param,
+        method: "get",
+    })
+}
+
+export const getFleetMembers = (param) => {
+    return axios.request({
+        url: "/api/fleet/get-fleet-members?" + param,
+        method: "get",
+    })
+}
+
+export const addDriver = (param) => {
+    return axios.request({
+        url: "/api/drivers/add-drivers",
+        method: "post",
+        data: param,
+    })
+}
+
+export const addStop = (param) => {
+    return axios.request({
+        url: "/api/line/add-stop",
+        method: "post",
+        data: param,
+    })
+}
+// special parameter (json)
+export const addLine = (param) => {
+    return axios.request({
+        url: "/api/line/add-new-line",
+        method: "post",
+        data: param,
+        header: {
+            'Content-Type': 'application/json'
+        }
+    })
+}
+
+export const addOneBus = (param) => {
+    return axios.request({
+        url: "/api/bus/add-one-bus",
+        method: "post",
+        data: param,
+    })
+}
+
+export const modifyDriverInfo = (param) => {
+    return axios.request({
+        url: "/api/driver/modify-driver-info",
+        method: "post",
+        data: param,
+    })
+}
+
+export const addViolationRecord = (param) => {
+    return axios.post({
+        url: "/api/violation/add-violation",
+        data: param,
+    })
+}
+
+export const setFleetCaptain = (param) => {
+    return axios.post({
+        url: "/api/line/set-fleet-captain",
+        data: param,
+    })
+}
+
+export const setLineCaptain = (param) => {
+    return axios.post({
+        url: "/api/line/set-line-captain",
+        data: param,
     })
 }
